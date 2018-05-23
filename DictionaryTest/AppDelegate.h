da67797e817bf7f5323038e11e094ce8339c8427
Admin <admin@mac-admin.local>
2018-05-23 19:40:21 +0300

    Initial Commit

---
+++ DictionaryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DictionaryTest
+//
+//  Created by Admin on 23.05.18.
+//  Copyright © 2018 SergioLechini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
